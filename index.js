8e775ea201242bd4a671ea545baa3e944193cdf7
LeBogo <lebogo@lebogo.me>
2023-02-27 19:14:32 +0100

    Your commit message

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 const nodegit = require("nodegit");
 nodegit.Repository.open("../Onu/onu2-server")
-  .then(function (repo) {
-    // Get the master branch
-    return repo.getMasterCommit();
-  })
-  .then(function (firstCommitOnMaster) {
-    // Create a new history event emitter
-    const history = firstCommitOnMaster.history();
+    .then(function (repo) {
+        // Get the master branch
+        return repo.getMasterCommit();
+    })
+    .then(function (firstCommitOnMaster) {
+        // Create a new history event emitter
+        const history = firstCommitOnMaster.history();
 
-    // Listen for commits on the event emitter
-    history.on("commit", function (commit) {
-      console.log(commit.sha());
-      console.log(commit.message());
-      console.log(
-        commit.author().name() + " <" + commit.author().email() + ">"
-      );
+        // Listen for commits on the event emitter
+        history.on("commit", function (commit) {
+            console.log(commit.sha());
+            console.log(commit.message());
+            console.log(
+                commit.author().name() + " <" + commit.author().email() + ">"
+            );
+        });
+
+        // Start emitting events
+        history.start();
+
+    })
+    .catch(function (err) {
+        console.log(err);
     });
-
-    // Start emitting events
-    history.start();
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
